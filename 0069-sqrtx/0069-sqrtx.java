7ce152b9146a39ada43f22e24ddc6cc3305a5356
Abhinav Kumar <avinavluv4@gmail.com>
2026-05-29 21:02:12 +0530

    Time: 76 ms (5.16%), Space: 42.7 MB (41.23%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,9 +2,9 @@ class Solution {
     public int mySqrt(int x) {
         int start=0;
         int end=x;
-        // if(x==0 || x==1){
-        //     return x;
-        // }
+        if(x==0 || x==1){
+            return x;
+        }
         while(start<=end){
             int mid=start+(end-start)/2;
             long square=(long)mid*mid;
