d766ddcee96acfb63f3d689c7c1642d40ec05d6c
Abhinav Kumar <avinavluv4@gmail.com>
2026-09-20 14:20:45 +0530

    Time: 5 ms (83.36%), Space: 45.9 MB (71.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         if(x<0){
+         if(x<0)
             return false;
-        }
         int ld=0;
         long rev=0;
         int i=x;
